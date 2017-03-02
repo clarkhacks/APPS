be226042fc4e2d7e8f7e91378a0fdf60ce679873
Clark Weckmann <clarkhacks@users.noreply.github.com>
2017-03-02 08:27:16 -0600

    Update index.js

--- bypass/js/index.js
+++ bypass/js/index.js
@@ -4,8 +4,5 @@ var qUrl = urlParams.get('url');
 var qRef = urlParams.get('ref');
 var qString = urlParams.toString();
 console.log(qUrl);
-  if (qref){
-    console.log(qRef);
-  };
 document.getElementById('qFrame').src = qUrl;
 }
