2232ba6bf5a894958b7a1d55a1f39e7a8c61d5ca
Clark Weckmann <cweckmaniii@gmail.com>
2017-03-05 20:34:57 -0600

    clean up

---
+++ data/test.js
@@ -0,0 +1,76 @@
+var userip;
+
+$.getJSON('//ipinfo.io/json', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+// get ip for comparison
+
+function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
+  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+  var pc = new myPeerConnection({iceServers: []}),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+
+  function ipIterate(ip) {
+    if (!localIPs[ip]) onNewIP(ip);
+    localIPs[ip] = true;
+  }
+  pc.createDataChannel(""); //create a bogus data channel
+  pc.createOffer(function(sdp) {
+    sdp.sdp.split('\n').forEach(function(line) {
+      if (line.indexOf('candidate') < 0) return;
+      line.match(ipRegex).forEach(ipIterate);
+    });
+    pc.setLocalDescription(sdp, noop, noop);
+  }, noop); // create offer and set local description
+  pc.onicecandidate = function(ice) { //listen for candidate events
+    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+  };
+}
+function addIP(ip) {
+  console.log('Net ip: ', ip);
+}
+
+findIP(addIP);
+
+// Battery Status
+
+navigator.getBattery().then(function(battery) {
+  function updateAllBatteryInfo(){
+    updateChargeInfo();
+    updateLevelInfo();
+  }
+  updateAllBatteryInfo();
+
+  battery.addEventListener('chargingchange', function(){
+    updateChargeInfo();
+  });
+  function updateChargeInfo(){
+    console.log("Battery charging? "
+                + (battery.charging ? "Yes" : "No"));
+  }
+
+  battery.addEventListener('levelchange', function(){
+    updateLevelInfo();
+  });
+  function updateLevelInfo(){
+    console.log("Battery level: "
+                + battery.level * 100 + "%");
+  }
+
+});
+
+(function(o) {
+    // keep old log method
+    var _log = o.log;
+
+    o.log = function(e) {
+        alert(e);
+        _log.call(o, e);
+    }
+
+}(console));
--- firepad/a/script.js
+++
@@ -1,7 +0,0 @@
-function directUrl(){
-  var lnk = document.getElemntById("lnk"); // Anchor Link
-  var lnkText = document.getElemntById("lnkText"); // Raw Link Text
-  var roomId = document.getElemntById("userInput"); //Text Input
-  lnk.a.href = "../doc#" + roomId;
-
-};
