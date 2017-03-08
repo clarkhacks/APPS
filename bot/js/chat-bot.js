7121cf75a20d19a5c3a1e0dfa81c75fc6320247f
Clark Weckmann <clarkhacks@users.noreply.github.com>
2017-03-08 09:07:34 -0600

    Update chat-bot.js

--- bot/js/chat-bot.js
+++ bot/js/chat-bot.js
@@ -40,8 +40,9 @@ function chatBot() {
 		if(this.input == 'noop'){
 			return;
 };
-				if(this.match('-- clear.')){
+		if(this.match('-- clear.')){
 			$( ".chatlines" ).remove();
+			return "Chat Cleared";	
 };
 		var unknown = ["I don't know what that means..", "Errr... Error? Does not computer.", "Dude. Idk what you're saying..."][Math.floor(Math.random() * 3)];
 		return unknown;
