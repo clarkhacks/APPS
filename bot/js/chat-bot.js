b34786d09c3a72f5554bbe93e36d46eaca45de8a
Clark Weckmann <clarkhacks@users.noreply.github.com>
2017-03-08 11:19:00 -0600

    Update chat-bot.js

--- bot/js/chat-bot.js
+++ bot/js/chat-bot.js
@@ -25,15 +25,16 @@ function chatBot() {
 			var responseTwo = ["Well, I'm building a robot", "Doing greaattt", "Weed. Weed. Weed. Smokey Smokey", "Smoking", "Weed", "Weeeeed", "Smoking some herrbb", "that herb", "Crystal Meth <3"][Math.floor(Math.random() * 10)]
 			return responseTwo;
 };
-		if(this.match('fuck*|shit|whore|slut|ass*|dick|cock|penis')){
+		if(this.match('fuck*|shit|whore|slut|ass*|dick|cock|penis|(n)+(i)+(g)+*')){
 			var responseThree = ["No need for profanity", "That's a bad fucking word", "Don't use that fucking language!"][Math.floor(Math.random() * 3)]
 			return responseThree;
 };
-		if(this.match('l(ol)+') || this.match('(ha)+(h|$)') || this.match('lmao')){
-			return "what's so funny?";
+		f(this.match('(l(ol)+|(l)+(m)+(a)+(o)+|(ha)+|f(u)+(n)+(y)+|x(D)+)')){
+			var responseFour = ["No need for profanity", "That's a bad fucking word", "Don't use that fucking language!"][Math.floor(Math.random() * 3)]
+			return responseFour;
 };
 		if(this.match('^no+(\\s|!|\\.|$)')){
-			return "don't be such a negative nancy :(";
+			return "no?";
 };
 		if(this.match('(cya|bye|see ya|ttyl|talk to you later)')){
 			return ["alright, see you around", "good teamwork!"];
@@ -44,14 +45,14 @@ function chatBot() {
 		if(this.input == 'noop'){
 			return;
 };
-		if(this.match('--clear|--c')){
+		if(this.match('(@c|@clear)')){
 			$( ".chatlines" ).remove();
 			return "Chat Cleared";	
 };
-		if(this.match('($ -help|$ -h)')){
-			return ["--clear : Clears Chat", "--help : Lists helful commands", "--reload : Reloads Chat"];	
+		if(this.match('(@help|@h)')){{
+			return ["@clear : Clears Chat", "@help : Lists helful commands", "@reload : Reloads Chat"];	
 };
-		if(this.match('(--reload|--r)')){
+		if(this.match('(@reload|@r)')){{
 		location.reload();
 };
 		var unknown = ["I don't know what that means..", "Errr... Error? Does not compute.", "Dude. Idk what you're saying...", "I'm not too smart to understand that yet. Whoops"][Math.floor(Math.random() * 4)];
