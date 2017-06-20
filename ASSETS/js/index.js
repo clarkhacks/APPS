6ad4026f1c6a4572da17c8f7a70797339e8d8e06
Clark Weckmann <cweckmaniii@gmail.com>
2017-06-20 14:38:06 -0500

    +

---
+++ ASSETS/js/index.js
@@ -0,0 +1,11 @@
+var $products = $("#searchable .row");
+$("#searchInput").keyup(function() {
+  var re = new RegExp($(this).val(), "i");
+  $products
+    .removeClass("hidden")
+    .filter(function() {
+      return !re.test($(this).text());
+    })
+    .addClass("hidden");
+});
+$( ".button:odd" ).addClass("button-primary");
