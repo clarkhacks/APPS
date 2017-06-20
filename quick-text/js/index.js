59620b576ffd3412547b38a9d4ed0439f9123c02
Clark Weckmann <cweckmaniii@gmail.com>
2017-06-20 10:27:24 -0500

    + quick-text

---
+++ quick-text/js/index.js
@@ -0,0 +1,12 @@
+var myDataRef = new Firebase('https://clarkhacks-db.firebaseio.com/quick-text');
+var textboxRef = myDataRef.child('textbox');
+var $textbox = $('#textbox');
+textboxRef.on('value', function(snapshot) {
+  console.log("From Firebase:", snapshot.val());
+  $textbox.val(snapshot.val().text);
+});
+$textbox.keyup(function() {
+  textboxRef.set({
+    text: $textbox.val()
+  });
+});
