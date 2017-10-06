79638f66a56ee3ea94ca24f43768d2fb50e74c2e
Clark Weckmann <cweckmaniii@gmail.com>
2017-10-05 23:34:55 -0500

    script test

--- tophone/js/index.js
+++ tophone/js/index.js
@@ -5,8 +5,8 @@
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("button").innerHTML = "SENT";
-        
+          document.getElementById("button").value = "SENT";
+
         }
       };
       xhttp.open(
